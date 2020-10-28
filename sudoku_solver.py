23252e617d3f68c0ad807b28d42d4a9b13f76b62
arthurvilar <ahc.vilar@gmail.com>
2020-10-28 11:54:36 -0300

    updated print_board function

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -1,7 +1,13 @@
-def draw_board(board):
-    for r in range(len(board)):
-        for c in range(len(board[r])):
-            print(f'{board[r][c]} ', end='')
+def print_board(board):
+    for i in range(len(board)):
+        if i % 3 == 0 and i != 0:
+            print('---------------------')
+
+        for j in range(len(board[i])):
+            if j % 3 == 0 and j != 0:
+                print('| ', end='')
+            print(f'{board[i][j]} ', end='')
+
         print()
 
 
@@ -10,15 +16,16 @@ def possible(row, col, n):
     global board
 
     # check row
-    if n in board[row]:
-        return False
+    for i in range(len(board[row])):
+        if board[row][i] == n:
+            return False
 
     # check column
     for i in range(len(board)):
         if board[i][col] == n:
             return False
 
-    # check square
+    # check box
     y = row//3 * 3
     x = col//3 * 3
     for i in range(3):
@@ -43,8 +50,8 @@ def solve():
                         board[i][j] = 0 # backtrack
                 return
 
-    print('\n----SOLUTION:----\n')
-    draw_board(board)
+    print('\n-=-=-=SOLUTION:=-=-=-\n')
+    print_board(board)
 
 
 def main():
@@ -62,8 +69,20 @@ def main():
         [0, 0, 4, 0, 2, 8, 5, 6, 1]
     ]
 
-    print('\n-----PUZZLE:-----\n')
-    draw_board(board)
+    '''board = [
+        [0, 0, 1, 0, 0, 6, 3, 0, 0],
+        [0, 3, 0, 8, 0, 0, 7, 2, 0],
+        [0, 0, 0, 4, 0, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0, 0, 0, 9],
+        [9, 0, 0, 7, 3, 0, 1, 0, 0],
+        [6, 0, 0, 0, 0, 0, 5, 0, 0],
+        [1, 9, 5, 0, 0, 0, 0, 0, 0],
+        [0, 0, 7, 0, 0, 8, 0, 0, 0],
+        [0, 0, 0, 1, 0, 0, 0, 0, 4]
+    ]'''
+
+    print('\n-=-=-=-PUZZLE:-=-=-=-\n')
+    print_board(board)
     solve()
 
 
