a4f4c585513803ec21b1ef3805dc24ad1b329c76
arthurvilar <ahc.vilar@gmail.com>
2020-10-27 22:58:20 -0300

    working

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -5,8 +5,51 @@ def draw_board(board):
         print()
 
 
+# check if number n can be placed on board[row][col]
+def possible(row, col, n):
+    global board
+
+    # check row
+    if n in board[row]:
+        return False
+
+    # check column
+    for i in range(len(board)):
+        if board[i][col] == n:
+            return False
+
+    # check square
+    y = row//3 * 3
+    x = col//3 * 3
+    for i in range(3):
+        for j in range(3):
+            if board[i+y][j+x] == n:
+                return False
+
+    return True
+
+
+# find a solution using backtracking
+def solve():
+    global board
+
+    for i in range(len(board)):
+        for j in range(len(board[i])):
+            if board[i][j] == 0:
+                for n in range(1, 10):
+                    if possible(i, j, n):
+                        board[i][j] = n
+                        solve()
+                        board[i][j] = 0 # backtrack
+                return
+
+    print('\n----SOLUTION:----\n')
+    draw_board(board)
+
+
 def main():
 
+    global board
     board = [
         [5, 0, 1, 0, 7, 2, 0, 0, 0],
         [6, 0, 9, 0, 0, 0, 0, 0, 0],
@@ -19,7 +62,9 @@ def main():
         [0, 0, 4, 0, 2, 8, 5, 6, 1]
     ]
 
+    print('\n-----PUZZLE:-----\n')
     draw_board(board)
+    solve()
 
 
 if __name__ == '__main__':
